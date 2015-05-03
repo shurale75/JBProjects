95a8fbb0b00a697cacf2388cf263f50970c962ba
shurale75 <shurale@gmail.com>
2015-05-03 17:41:35 +0300

    aaa111

--- rent_1/App_Start/RouteConfig.cs
+++ rent_1/App_Start/RouteConfig.cs
@@ -14,6 +14,7 @@ namespace rent_1
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
